7be30869663405818e6e8da62f28f620562962ad
Timilehin Olusa <olusatimmie07@gmail.com>
2023-08-16 23:59:22 +0100

    Updating

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,7 +17,24 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ returns a random key """
         key = str(uuid.uuid4())
-        if type(data) == bytes:
+        if isinstance(key, bytes):
             data = data.decode('utf-8')
         self._redis.set(key, data)
         return key
+
+    @staticmethod
+    def get_str(arg: bytes) -> str:
+        """ converts the byte object to a string """
+        return str(arg)
+
+    @staticmethod
+    def get_int(arg: bytes) -> int:
+        """ converts the byte object to an int """
+        return int(arg)
+
+    def get(self, key: str, fn=None):
+        """ converts data(key) back to the required format(fn) """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
