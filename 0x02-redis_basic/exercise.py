6d413ede1326d3ad437ef11b9aa8fbf6d11a56f2
Timilehin Olusa <olusatimmie07@gmail.com>
2023-08-16 22:48:37 +0100

    updating

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This is a python module
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """" This is a cache class"""
+    def __init__(self):
+        """ initializes this class """
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ returns a random key """
+        key = str(uuid.uuid4())
+        data = data.decode('utf-8')
+        self._redis.set(key, data)
+        return key
