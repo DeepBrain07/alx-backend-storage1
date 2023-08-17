a879ba111f23473e6e551804f7748058d529e27e
Timilehin Olusa <olusatimmie07@gmail.com>
2023-08-17 05:10:44 +0100

    Updating

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ returns a callable """
+    func_name = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, arg):
+        """ class wrapper """
+        self._redis.rpush(f'{func_name}:inputs', str(arg))
+        key = str(uuid.uuid4())
+        self._redis.rpush(f'{func_name}:outputs', key)
+        if isinstance(key, bytes):
+            arg = arg.decode('utf-8')
+        self._redis.set(key, arg)
+        return key
+    return wrapper
+
+
 class Cache:
     """" This is a cache class"""
     def __init__(self):
@@ -28,6 +45,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ returns a random key """
         key = str(uuid.uuid4())
